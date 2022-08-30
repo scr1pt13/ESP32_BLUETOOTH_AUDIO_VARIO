a9b90624a22867a8b09bb79128db8a52ed7c1896
Paragliding FM <mymail@my.local>
2022-08-30 14:34:22 +0200

    changed the sound

--- lib/vaudio/vaudio.cpp
+++ lib/vaudio/vaudio.cpp
@@ -31,11 +31,6 @@ static int32_t CrossoverCps;
 static int BeepPeriodTicks; // internal state : current beep interval in ticks
 static int BeepEndTick; // internal state : current beep  on-time in ticks
 static int Tick; // internal state : current tick ( 1 tick ~= 20mS)
-static int VarioState; // internal state : climb/zeroes/quiet/sink
-// for offscale climbrates above +10m/s generate continuous warbling tone
-static const int OffScaleHiTone[8]  = {400,800,1200,1600,2000,1600,1200,800};
-// for offscale sinkrates below -10m/s generate continuous descending tone
-static const int OffScaleLoTone[8]  = {4000,3500,3000,2500,2000,1500,1000,500};
 // {beep_period, beep_on_time} based on vertical climbrate in 1m/s intervals
 static const BEEP BeepTbl[10] = {
 {16,10}, // 0m/s to +1m/s
@@ -77,31 +72,14 @@ static void vaudio_reset(int32_t nCps) {
 	Tick = 0;
 	// if sinking significantly faster than glider sink rate in still air, generate warning sink tone
 	if (CurrentCps <= SinkToneCps) {
-		if (CurrentCps <= -VARIO_MAX_CPS) {
-			BeepPeriodTicks = 8;
-			BeepEndTick = 8;
-			CurrentFreqHz = OffScaleLoTone[0];
+			BeepPeriodTicks = 40; 
+			BeepEndTick  = 30;			
+			CurrentFreqHz = 220;
 			audio_set_frequency(CurrentFreqHz);
-			}
-		else {
-			BeepPeriodTicks = 40; // sink indicated with descending frequency beeps with long on-times
-			BeepEndTick  = 30;
-			// descending tone starts at higher frequency for higher sink rate
-			CurrentFreqHz = VARIO_SPKR_MAX_FREQHZ/2 + ((CurrentCps + VARIO_MAX_CPS)*(VARIO_SPKR_MIN_FREQHZ + 600 - VARIO_SPKR_MAX_FREQHZ/2))/(SinkToneCps + VARIO_MAX_CPS);
-			CLAMP(CurrentFreqHz, VARIO_SPKR_MIN_FREQHZ, VARIO_SPKR_MAX_FREQHZ);
-			audio_set_frequency(CurrentFreqHz);
-			}
-		}
+			}		
 	//if climbing, generate beeps
 	else {
 		if (CurrentCps >= ClimbToneCps) {
-			if (CurrentCps >= VARIO_MAX_CPS) {
-				BeepPeriodTicks = 8;
-				BeepEndTick = 8;
-				CurrentFreqHz = OffScaleHiTone[0];
-				audio_set_frequency(CurrentFreqHz);
-				}
-			else {
 				int index = CurrentCps/100;
 				if (index > 9) index = 9;
 				BeepPeriodTicks = BeepTbl[index].periodTicks;
@@ -113,15 +91,13 @@ static void vaudio_reset(int32_t nCps) {
 					CurrentFreqHz = VARIO_SPKR_MIN_FREQHZ + ((CurrentCps - ZeroesToneCps)*(VARIO_CROSSOVER_FREQHZ - VARIO_SPKR_MIN_FREQHZ))/(CrossoverCps - ZeroesToneCps);
 					}
 				CLAMP(CurrentFreqHz, VARIO_SPKR_MIN_FREQHZ, VARIO_SPKR_MAX_FREQHZ);
-				audio_set_frequency(CurrentFreqHz);
-				}
+				audio_set_frequency(CurrentFreqHz);				
 			}
 		else   // in "zeroes" band, indicate with a short pulse and long interval
 		if (CurrentCps >= ZeroesToneCps) {
-			BeepPeriodTicks = 30;
-			BeepEndTick = 2;
-			CurrentFreqHz = VARIO_SPKR_MIN_FREQHZ + ((CurrentCps - ZeroesToneCps)*(VARIO_CROSSOVER_FREQHZ - VARIO_SPKR_MIN_FREQHZ))/(CrossoverCps - ZeroesToneCps);
-			CLAMP(CurrentFreqHz, VARIO_SPKR_MIN_FREQHZ, VARIO_SPKR_MAX_FREQHZ);
+			BeepPeriodTicks = 60;
+			BeepEndTick = 10;
+			CurrentFreqHz = 300;
 			audio_set_frequency(CurrentFreqHz);
 			}            
 		// between zeroes threshold and sink threshold, chillout
@@ -146,22 +122,10 @@ void vaudio_tick_handler(int32_t nCps) {
         BeepPeriodTicks--;
         if (Tick >= BeepEndTick) { // shut off climb beep after 'on' time ends
             newFreqHz = 0;
-            }
-        else
-        if (CurrentCps >= VARIO_MAX_CPS) { // offscale climbrate (>= +10m/s) indicated with continuous warbling tone
-            newFreqHz = OffScaleHiTone[Tick];
-            }
-        else
-        if (CurrentCps <= -VARIO_MAX_CPS) {  // offscale sink (<= -10m/s) indicated with continuous descending tone
-            newFreqHz = OffScaleLoTone[Tick];
-            }
-        else
-        if (CurrentCps <= SinkToneCps) {  // sink is indicated with a descending frequency beep
-            newFreqHz = CurrentFreqHz - 20;
-            }
+        }
         else {
             newFreqHz = CurrentFreqHz; // no change   
-            }
+        }
         if (newFreqHz != CurrentFreqHz) {
             CurrentFreqHz = newFreqHz;
             audio_set_frequency(CurrentFreqHz);
